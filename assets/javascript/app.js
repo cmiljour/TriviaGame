c18a6480dd762525031fe36bb82f17563e8c144a
cmiljour <cormil@gmail.com>
2017-06-04 13:41:04 -0500

    Close to Final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,9 @@
+
+var correct = 0;
+var incorrect = 0;
+var notSelected = 0;
+var seconds = 60;
+
 window.onload = function (){
 $("#hideForm").hide();
 $("#results").hide();
@@ -11,71 +17,63 @@ function secondPassed() {
     }
     document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
     if (seconds == 0) {
-        clearInterval(countdownTimer);
-        document.getElementById('right').innerHTML = correct;
-        document.getElementById('wrong').innerHTML = incorrect;
-        document.getElementById('noShow').innerHTML = notSelected;
+    checkRadio();
+    clearTimeout(countdownTimer);
+    $("#hideForm").hide();
+    $("#results").show();
+    $("#right").text(correct);
+    $("#wrong").text(incorrect);
+    $("#noShow").text(notSelected);
 
     } else {
         seconds--;
     }
 }
 
+function checkRadio () {
+    $('input:radio').each(function() {
+    if($(this).is(':checked')) {
+    // You have a checked radio button here...
+        if (this.value === "1"){
+            correct++;
+        }
+        else if (this.value === "0"){
+            incorrect++;
+        }
+        
+    }
+    else if(!$(this).is(':checked')) {
+        notSelected++;
+        }
+    });
+}
 
-var correct = 0;
-var incorrect = 0;
-var notSelected = 0;
-var seconds = 60;
-var countdownTimer = setInterval(secondPassed, 1000);
+
+var countdownTimer;
 
 $(document).ready( function() {
+
 $("#start").click(function() {
     $("#hideForm").show();
     $("#start").hide();
-
 });
 
-function checkRadio () {
-	$('input:radio').each(function() {
-  	if($(this).is(':checked')) {
-    // You have a checked radio button here...
-    	if (this.value === "1"){
-    		correct++;
-    	}
-    	else if (this.value === "0"){
-    		incorrect++;
-    	}
-    	
-  	}
-  	else if(!$(this).is(':checked')) {
-    	notSelected++;
-    	}
-	});
-}
-
-// setTimeout(checkRadio, 5000);
-notSelected = notSelected - 4;
-
-$('#myButton2').on('click', function () {
-    clearInterval(countdownTimer);
-    document.body.innerHTML = "";
-    document.getElementById('right').innerHTML = correct;
-    document.getElementById('wrong').innerHTML = incorrect;
-    document.getElementById('noShow').innerHTML = notSelected;
-})
-
-
-
 $('#myButton').on('click', function () {
-    secondPassed();  
+    countdownTimer = setInterval(secondPassed, 1000);
+      
 })
 
-$('#myButton2').on('click', function () {
-    document.body.innerHTML = "";
-    document.getElementById('right').innerHTML = correct;
-    document.getElementById('wrong').innerHTML = incorrect;
-    document.getElementById('noShow').innerHTML = notSelected;
+$('#myButton2').click(function () {
+    checkRadio();
+    clearTimeout(countdownTimer);
+    $("#hideForm").hide();
+    $("#results").show();
+    $("#right").text(correct);
+    $("#wrong").text(incorrect);
+    $("#noShow").text(notSelected);
 })
+
+notSelected = notSelected - 4;
 
 });
 

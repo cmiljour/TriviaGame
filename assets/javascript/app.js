b5c7527eb88b0ece164605e551540eff4ec9593b
cmiljour <cormil@gmail.com>
2017-06-05 13:37:10 -0500

    rearranged code and added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,37 @@ var correct = 0;
 var incorrect = 0;
 var notSelected = 0;
 var seconds = 60;
+var countdownTimer;
 
-window.onload = function (){
-$("#hideForm").hide();
-$("#results").hide();
+// check what radios are selected and update variables with value count
+function checkRadio () {
+    $('input:radio').each(function() {
+    if($(this).is(':checked')) {
+        if (this.value === "1"){
+            correct++;
+        }
+        else if (this.value === "0"){
+            incorrect++;
+        }     
+    }
+        else if(!$(this).is(':checked')) {
+            notSelected++;
+        }
+    });
 }
 
+// once game ends, parse radios (checkRadio), end countdown timer, and display results
+function endGame() {
+    checkRadio();
+    clearTimeout(countdownTimer);
+    $("#hideForm").hide();
+    $("#results").show();
+    $("#right").text(correct);
+    $("#wrong").text(incorrect);
+    $("#noShow").text(notSelected);
+}
+
+// create countdown timer text
 function secondPassed() {
     var minutes = Math.round((seconds - 30)/60);
     var remainingSeconds = seconds % 60;
@@ -16,64 +41,35 @@ function secondPassed() {
         remainingSeconds = "0" + remainingSeconds;  
     }
     document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+    // If timer counts all the way down, parse radios (checkRadio), end countdown timer, and display results
     if (seconds == 0) {
-    checkRadio();
-    clearTimeout(countdownTimer);
-    $("#hideForm").hide();
-    $("#results").show();
-    $("#right").text(correct);
-    $("#wrong").text(incorrect);
-    $("#noShow").text(notSelected);
-
-    } else {
+        endGame();
+    } 
+    else {
         seconds--;
     }
 }
 
-function checkRadio () {
-    $('input:radio').each(function() {
-    if($(this).is(':checked')) {
-    // You have a checked radio button here...
-        if (this.value === "1"){
-            correct++;
-        }
-        else if (this.value === "0"){
-            incorrect++;
-        }
-        
-    }
-    else if(!$(this).is(':checked')) {
-        notSelected++;
-        }
-    });
-}
-
-
-var countdownTimer;
-
 $(document).ready( function() {
-
-$("#start").click(function() {
-    $("#hideForm").show();
-    $("#start").hide();
-});
-
-$('#myButton').on('click', function () {
-    countdownTimer = setInterval(secondPassed, 1000);
-      
-})
-
-$('#myButton2').click(function () {
-    checkRadio();
-    clearTimeout(countdownTimer);
+    //show only a start button
     $("#hideForm").hide();
-    $("#results").show();
-    $("#right").text(correct);
-    $("#wrong").text(incorrect);
-    $("#noShow").text(notSelected);
-})
+    $("#results").hide();
+    // show quiz questions after clicking the start button
+    $("#start").click(function() {
+        $("#hideForm").show();
+        $("#start").hide();
+    });
+    // start the game and timer
+    $('#myButton').on('click', function () {
+    countdownTimer = setInterval(secondPassed, 1000);    
+    })
+    
+    // end timer button 
+    $('#myButton2').click(function () {
+        endGame();
+    })
 
-notSelected = notSelected - 4;
+    notSelected = notSelected - 4;
 
 });
 
